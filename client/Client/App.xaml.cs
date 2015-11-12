f44a21505e621a783bca30b9bbaae9544d846993
Guuugol <abcd666@bk.ru>
2015-11-12 22:28:24 +0300

    some code fn client

--- client/Client/App.xaml.cs
+++ client/Client/App.xaml.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
 using System.Windows;
+using Client.Server;
+
 
 namespace WpfApplication1
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
+    /// 
+    
     public partial class App : Application
     {
+        public Client.Server.ServiceSoapClient serverClient;
+        //private static ServiceSoapClient cl = new ServiceSoapClient();  
+        public App() : base()
+         {
+            
+            serverClient = new ServiceSoapClient(); //.Authorize("111", "1111");
+           /* serverClient.Authorize("123", "123");*/
+        }
+
 
     }
 }
